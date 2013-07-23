427ec76e3d01a22065e76d2474457bd06f06230a
JUAN JOSE MONTES DE OCA ARBOS <juanjose.montesdeocaarbos@gmail.com>
2013-07-23 14:11:46 -0300

    Startup project

---
+++ src/mBlogEngine.Domain.Tests/PostTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace mBlogEngine.Domain.Tests
+{
+	[TestFixture]
+    public class PostTests
+    {
+		[Test]
+		public void CrearUnPostBasico()
+		{
+			var post = new Post("Título") {Text = "Primer post"};
+
+			Assert.AreEqual("Título", post.Title);
+			Assert.AreEqual("Primer post", post.Text);
+			Assert.AreEqual(null, post.Summary);
+		}
+    }
+}
---
+++ src/mBlogEngine.Domain/Post.cs
@@ -0,0 +1,16 @@
+﻿namespace mBlogEngine.Domain
+{
+    public class Post
+    {
+		public Post(string title)
+	    {
+		    Title = title;
+	    }
+
+	    public string Title { get; set; }
+		
+		public string Text { get; set; }
+
+	    public string Summary { get; set; }
+    }
+}
