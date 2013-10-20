8b263e9f6d8b3ff1b7ddd224b81385c2cef892cc
JUAN JOSE MONTES DE OCA ARBOS <juanjose.montesdeocaarbos@gmail.com>
2013-10-20 03:24:15 -0300

    Add basic test to PostController.Index

---
+++ src/mBlogEngine.Admin.Tests/PostController.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using mBlogEngine.Admin.Controllers;
+
+namespace mBlogEngine.AdminWeb.Tests
+{
+	[TestFixture]
+	public class PostControllerTests
+	{
+		[Test]
+		public void PostIndex()
+		{
+			var controller = new PostController();
+			var actionResult = controller.Index();
+
+			Assert.IsNotNull(actionResult);
+		}
+	}
+}
---
+++ src/mBlogEngine.Admin/Controllers/PostController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace mBlogEngine.Admin.Controllers
+{
+    public class PostController : Controller
+    {
+        //
+        // GET: /Post/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
