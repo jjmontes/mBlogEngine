cfce373012d496498c2c15bffef4533082373abf
JUAN JOSE MONTES DE OCA ARBOS <juanjose.montesdeocaarbos@gmail.com>
2013-10-20 03:19:18 -0300

    Adding Web Test Project

---
+++ src/mBlogEngine.Admin.Tests/SetupControllerTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using mBlogEngine.Admin.Controllers;
+
+namespace mBlogEngine.AdminWeb.Tests
+{
+	[TestFixture]
+    public class SetupControllerTests
+    {
+		[Test]
+		public void SetupIndex()
+		{
+			var controller = new SetupController();
+			var actionResult = controller.Index();
+
+			Assert.IsNotNull(actionResult);
+		}
+    }
+}
