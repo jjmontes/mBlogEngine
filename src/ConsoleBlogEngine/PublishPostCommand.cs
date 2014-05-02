e89bf708ea6fb36e308aba980fe819c22ffd1b57
JUAN JOSE MONTES DE OCA ARBOS <juanjose.montesdeocaarbos@gmail.com>
2014-05-02 13:06:15 -0300

    Add test to pubish post when post's file doesn't exist

---
+++ src/ConsoleBlogEngine/PublishPostCommand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleBlogEngine
+{
+	public class PublishPostCommand
+	{
+		private readonly Action<string> _writer;
+
+		public PublishPostCommand(Action<string> writer)
+		{
+			_writer = writer;
+		}
+
+		public void Execute(string args)
+		{
+			_writer.Invoke(string.Format("cbe publish {0}", args));
+			_writer.Invoke("File 'post.txt' doesn't exist. Try to add path to file. Example: -f:C:\\blog\\post.txt");
+		}
+	}
+}
---
+++ src/mBlogEngine.Console.Tests/PostCommandTests.cs
@@ -0,0 +1,20 @@
+﻿using ConsoleBlogEngine;
+using NUnit.Framework;
+
+namespace mBlogEngine.Console.Tests
+{
+	[TestFixture]
+	public class PostCommandTests
+	{
+		[Test]
+		public void PublishPostWhenFileDoesntExist()
+		{
+			var command = new PublishPostCommand(ConsoleStub.WriteLine);
+
+			command.Execute("-f:post.txt");
+
+			StringAssert.Contains("cbe publish -f:post.txt", ConsoleStub.Text);
+			StringAssert.Contains("File 'post.txt' doesn't exist. Try to add path to file. Example: -f:C:\\blog\\post.txt", ConsoleStub.Text);
+		}
+	}
+}
