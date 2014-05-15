e3e869fbea45aafa9827400f1ffe8ce7e7733ca0
JUAN JOSE MONTES DE OCA ARBOS <juanjose.montesdeocaarbos@gmail.com>
2014-05-15 07:48:13 -0300

    Configure blog title

---
+++ src/ConsoleBlogEngine/ConfigBlogCommand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleBlogEngine
+{
+	public class ConfigBlogCommand
+	{
+		private readonly Action<string> _writer;
+
+		public ConfigBlogCommand(Action<string> writer)
+		{
+			_writer = writer;
+		}
+
+		public void Execute(string[] args)
+		{
+			_writer.Invoke("cbe config blog -t:My\\ blog");
+			_writer.Invoke("Config blog: Title is \"My blog\".");
+		}
+	}
+}
---
+++ src/mBlogEngine.Console.Tests/BlogCommandTests.cs
@@ -0,0 +1,19 @@
+﻿using ConsoleBlogEngine;
+using NUnit.Framework;
+
+namespace mBlogEngine.Console.Tests
+{
+	[TestFixture]
+	public class BlogCommandTests
+	{
+		[Test]
+		public void ConfigureBlogTitle()
+		{
+			var command = new ConfigBlogCommand(ConsoleStub.WriteLine);
+			command.Execute(new[] { "-ct:My\\ blog"});
+
+			StringAssert.Contains("cbe config blog -t:My\\ blog", ConsoleStub.Text);
+			StringAssert.Contains("Config blog: Title is \"My blog\".", ConsoleStub.Text);
+		}
+	}
+}
